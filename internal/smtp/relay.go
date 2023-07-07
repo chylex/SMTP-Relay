31b9c46f9976c7d95529c5a16f496e8cd9bfdc83
chylex <contact@chylex.com>
2023-07-07 08:55:43 +0200

    Replace From/To headers in relayed emails

--- internal/smtp/relay.go
+++ internal/smtp/relay.go
@@ -128,16 +128,21 @@ func mailHandler(cfg *config.Config, log *logrus.Logger) func(peer smtpd.Peer, e
 			peerIP = addr.IP.String()
 		}
 
+		env.AddReceivedLine(peer)
+
+		senderAddr := env.Sender
+		senderName := fmt.Sprintf("%s <%s>", peer.Username, senderAddr)
+		recipients := env.Recipients
+		message := []byte(replaceHeaders(string(env.Data), senderName, recipients))
+
 		logger := log.WithFields(
 			logrus.Fields{
 				"account": peer.Username,
-				"from":    env.Sender,
-				"to":      env.Recipients,
+				"from":    senderAddr,
+				"to":      recipients,
 				"uuid":    generateUUID(log),
 			},
 		)
-
-		env.AddReceivedLine(peer)
 
 		if *cfg.Command != "" {
 			cmdLogger := logger.WithField("command", *cfg.Command)
@@ -146,8 +151,8 @@ func mailHandler(cfg *config.Config, log *logrus.Logger) func(peer smtpd.Peer, e
 			var stderr bytes.Buffer
 
 			environ := os.Environ()
-			environ = append(environ, fmt.Sprintf("%s=%s", "SMTPRELAY_FROM", env.Sender))
-			environ = append(environ, fmt.Sprintf("%s=%s", "SMTPRELAY_TO", env.Recipients))
+			environ = append(environ, fmt.Sprintf("%s=%s", "SMTPRELAY_FROM", senderAddr))
+			environ = append(environ, fmt.Sprintf("%s=%s", "SMTPRELAY_TO", recipients))
 			environ = append(environ, fmt.Sprintf("%s=%s", "SMTPRELAY_PEER", peerIP))
 
 			cmd := exec.Cmd{
@@ -155,7 +160,7 @@ func mailHandler(cfg *config.Config, log *logrus.Logger) func(peer smtpd.Peer, e
 				Path: *cfg.Command,
 			}
 
-			cmd.Stdin = bytes.NewReader(env.Data)
+			cmd.Stdin = bytes.NewReader(message)
 			cmd.Stdout = &stdout
 			cmd.Stderr = &stderr
 
@@ -177,13 +182,7 @@ func mailHandler(cfg *config.Config, log *logrus.Logger) func(peer smtpd.Peer, e
 		logger = logger.WithField("host", account.Remote.Addr)
 		logger.Info("delivering mail from peer using smarthost")
 
-		err := SendMail(
-			&account.Remote,
-			*cfg.Hostname,
-			env.Sender,
-			env.Recipients,
-			env.Data,
-		)
+		err := SendMail(&account.Remote, *cfg.Hostname, senderAddr, recipients, message)
 		if err != nil {
 			var smtpError smtpd.Error
 
@@ -253,4 +252,58 @@ func addressAllowedByTemplate(allowedAddresses []string, addr string) bool {
 func addressAllowedByRegex(allowedRegex *regexp.Regexp, addr string) bool {
 	// If not set, allow all addresses
 	return allowedRegex == nil || allowedRegex.MatchString(addr)
+}
+
+func replaceHeaders(data string, sender string, recipients []string) string {
+	lines := strings.Split(data, "\n")
+
+	isReadingHeader := true
+	hasFromHeader := false
+	hasToHeader := false
+
+	var builder strings.Builder
+	for index, line := range lines {
+		line = strings.TrimRight(line, "\r")
+
+		if isReadingHeader {
+			if strings.HasPrefix(line, "From:") {
+				writeFromHeader(&builder, sender)
+				hasFromHeader = true
+				continue
+			} else if strings.HasPrefix(line, "To:") {
+				writeToHeader(&builder, recipients)
+				hasToHeader = true
+				continue
+			} else if line == "" {
+				isReadingHeader = false
+
+				if !hasFromHeader {
+					writeFromHeader(&builder, sender)
+				}
+
+				if !hasToHeader {
+					writeToHeader(&builder, recipients)
+				}
+			}
+		}
+
+		builder.WriteString(line)
+		if index < len(lines)-1 {
+			builder.WriteString("\r\n")
+		}
+	}
+
+	return builder.String()
+}
+
+func writeFromHeader(builder *strings.Builder, from string) {
+	builder.WriteString("From: ")
+	builder.WriteString(from)
+	builder.WriteString("\r\n")
+}
+
+func writeToHeader(builder *strings.Builder, recipients []string) {
+	builder.WriteString("To: ")
+	builder.WriteString(strings.Join(recipients, ", "))
+	builder.WriteString("\r\n")
 }
--- internal/smtp/relay_test.go
+++ internal/smtp/relay_test.go
@@ -92,3 +92,40 @@ func TestAddrAllowedMixed(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+func TestReplaceHeadersReplacesExisting(t *testing.T) {
+	original := "Subject: abc\r\nFrom: me\r\nTo: you\r\n\r\nbody"
+	expected := "Subject: abc\r\nFrom: not-me\r\nTo: not-you\r\n\r\nbody"
+	testReplaceHeaders(t, "not-me", []string{"not-you"}, original, expected)
+}
+
+func TestReplaceHeadersAddsMissingFrom(t *testing.T) {
+	original := "Subject: abc\r\nTo: you\r\n\r\nbody"
+	expected := "Subject: abc\r\nTo: you\r\nFrom: me\r\n\r\nbody"
+	testReplaceHeaders(t, "me", []string{"you"}, original, expected)
+}
+
+func TestReplaceHeadersAddsMissingTo(t *testing.T) {
+	original := "Subject: abc\r\nFrom: me\r\n\r\nbody"
+	expected := "Subject: abc\r\nFrom: me\r\nTo: you\r\n\r\nbody"
+	testReplaceHeaders(t, "me", []string{"you"}, original, expected)
+}
+
+func TestReplaceHeadersAddsMissingBoth(t *testing.T) {
+	original := "Subject: abc\r\n\r\nbody"
+	expected := "Subject: abc\r\nFrom: me\r\nTo: you\r\n\r\nbody"
+	testReplaceHeaders(t, "me", []string{"you"}, original, expected)
+}
+
+func TestReplaceHeadersSupportsMultipleRecipients(t *testing.T) {
+	original := "Subject: abc\r\nFrom: me\r\n\r\nbody"
+	expected := "Subject: abc\r\nFrom: me\r\nTo: a, b, c\r\n\r\nbody"
+	testReplaceHeaders(t, "me", []string{"a", "b", "c"}, original, expected)
+}
+
+func testReplaceHeaders(t *testing.T, sender string, recipients []string, original string, expected string) {
+	actual := replaceHeaders(original, sender, recipients)
+	if actual != expected {
+		t.Errorf("Expected: %v\n\nGot: %v", expected, actual)
+	}
+}
